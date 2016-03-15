7282af76458b1a64174b5ccef2bebc018f296039
Gabe Sumner <gabe.sumner@gmail.com>
2016-03-15 09:21:36 -0700

    Update Apply.java

--- src/main/java/Apply.java
+++ src/main/java/Apply.java
@@ -19,6 +19,8 @@ public class Apply {
         Apply apply = new Apply();
         apply.JobID = jobID;
         apply.Applicant = applicant;
+        
+        // Commment
 
         System.out.println("Getting authorization token ----");
         apply.getAccessToken();
